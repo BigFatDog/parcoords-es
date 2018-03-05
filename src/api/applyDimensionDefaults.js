9e4bc7e98331c3254aee4cadf7e6a8320519b410
Xing Yun <ink.xing.yun@gmail.com>
2018-03-05 09:02:40 -0500

    refactor

--- src/api/applyDimensionDefaults.js
+++ src/api/applyDimensionDefaults.js
@@ -2,28 +2,25 @@ import { keys } from 'd3-collection';
 
 const applyDimensionDefaults = (config, pc) =>
   function(dims) {
-    let types = pc.detectDimensionTypes(config.data);
+    const types = pc.detectDimensionTypes(config.data);
     dims = dims ? dims : keys(types);
-    let newDims = {};
-    let currIndex = 0;
-    dims.forEach(function(k) {
-      newDims[k] = config.dimensions[k] ? config.dimensions[k] : {};
-      //Set up defaults
-      newDims[k].orient = newDims[k].orient ? newDims[k].orient : 'left';
-      newDims[k].ticks = newDims[k].ticks != null ? newDims[k].ticks : 5;
-      newDims[k].innerTickSize =
-        newDims[k].innerTickSize != null ? newDims[k].innerTickSize : 6;
-      newDims[k].outerTickSize =
-        newDims[k].outerTickSize != null ? newDims[k].outerTickSize : 0;
-      newDims[k].tickPadding =
-        newDims[k].tickPadding != null ? newDims[k].tickPadding : 3;
-      newDims[k].type = newDims[k].type ? newDims[k].type : types[k];
 
-      newDims[k].index =
-        newDims[k].index != null ? newDims[k].index : currIndex;
-      currIndex++;
-    });
-    return newDims;
+    return dims.reduce((acc, cur, i)=> {
+        const k = config.dimensions[cur] ? config.dimensions[cur] : {};
+
+        acc[cur] = {
+            ...k,
+            orient: k.orient ? k.orient : 'left',
+            ticks: k.ticks != null ? k.ticks : 5,
+            innerTickSize: k.innerTickSize != null ? k.innerTickSize : 6,
+            outerTickSize: k.outerTickSize != null ? k.outerTickSize : 0,
+            tickPadding: k.tickPadding != null ? k.tickPadding : 3,
+            type: k.type ? k.type : types[cur],
+            index: k.index != null ? k.index : i,
+        }
+
+        return acc;
+    }, {});
   };
 
 export default applyDimensionDefaults;
