bbee6eedd5e2d0d4c1dfd3baa5df8c3c7c47a4dd
Xing Yun <ink.xing.yun@gmail.com>
2018-03-25 17:41:19 -0400

    coding style

--- src/brush/multi/install.js
+++ src/brush/multi/install.js
@@ -8,34 +8,32 @@ const install = (state, config, pc, events) => () => {
     pc.createAxes();
   }
 
+  // Add and store a brush for each axis.
+  const brush = g
+    .append('svg:g')
+    .attr('class', 'brush')
+    .each(function(d) {
+      select(this).call(brushFor(state, config, pc, events)(d));
+    });
 
-    // Add and store a brush for each axis.
-    const brush = g.append("svg:g")
-        .attr("class", "brush")
-        .each(function(d) {
-            select(this).call(
-                brushFor(state, config, pc, events)(d)
-            );
-        })
+  brush
+    .selectAll('rect')
+    .style('visibility', null)
+    .attr('x', -15)
+    .attr('width', 30);
 
-    brush.selectAll("rect")
-        .style("visibility", null)
-        .attr("x", -15)
-        .attr("width", 30);
+  brush.selectAll('rect.background').style('fill', 'transparent');
 
-    brush.selectAll("rect.background")
-        .style("fill", "transparent");
+  brush
+    .selectAll('rect.extent')
+    .style('fill', 'rgba(255,255,255,0.25)')
+    .style('stroke', 'rgba(0,0,0,0.6)');
 
-    brush.selectAll("rect.extent")
-        .style("fill", "rgba(255,255,255,0.25)")
-        .style("stroke", "rgba(0,0,0,0.6)");
+  brush.selectAll('.resize rect').style('fill', 'rgba(0,0,0,0.1)');
 
-    brush.selectAll(".resize rect")
-        .style("fill", "rgba(0,0,0,0.1)");
-
-    pc.brushExtents = brushExtents(state, config, pc);
-    pc.brushReset = brushReset(state, config, pc);
-    return pc;
+  pc.brushExtents = brushExtents(state, config, pc);
+  pc.brushReset = brushReset(state, config, pc);
+  return pc;
 };
 
 export default install;
--- src/brush/multi/selected.js
+++ src/brush/multi/selected.js
@@ -17,41 +17,55 @@ const selected = (state, config, brushGroup) => () => {
   if (actives.length === 0) return config.data;
 
   // test if within range
-    const within = {
-        "date": function(d,p,dimension,b) {
-            if (typeof config.dimensions[p].yscale.rangePoints === "function") { // if it is ordinal
-                return b[0] <= config.dimensions[p].yscale(d[p]) && config.dimensions[p].yscale(d[p]) <= b[1]
-            } else {
-                return b[0] <= d[p] && d[p] <= b[1]
-            }
-        },
-        "number": function(d,p,dimension,b) {
-            if (typeof config.dimensions[p].yscale.rangePoints === "function") { // if it is ordinal
-                return b[0] <= config.dimensions[p].yscale(d[p]) && config.dimensions[p].yscale(d[p]) <= b[1]
-            } else {
-                return b[0] <= d[p] && d[p] <= b[1]
-            }
-        },
-        "string": function(d,p,dimension,b) {
-            return b[0] <= config.dimensions[p].yscale(d[p]) && config.dimensions[p].yscale(d[p]) <= b[1]
-        }
-    };
+  const within = {
+    date: function(d, p, dimension, b) {
+      if (typeof config.dimensions[p].yscale.rangePoints === 'function') {
+        // if it is ordinal
+        return (
+          b[0] <= config.dimensions[p].yscale(d[p]) &&
+          config.dimensions[p].yscale(d[p]) <= b[1]
+        );
+      } else {
+        return b[0] <= d[p] && d[p] <= b[1];
+      }
+    },
+    number: function(d, p, dimension, b) {
+      if (typeof config.dimensions[p].yscale.rangePoints === 'function') {
+        // if it is ordinal
+        return (
+          b[0] <= config.dimensions[p].yscale(d[p]) &&
+          config.dimensions[p].yscale(d[p]) <= b[1]
+        );
+      } else {
+        return b[0] <= d[p] && d[p] <= b[1];
+      }
+    },
+    string: function(d, p, dimension, b) {
+      return (
+        b[0] <= config.dimensions[p].yscale(d[p]) &&
+        config.dimensions[p].yscale(d[p]) <= b[1]
+      );
+    },
+  };
 
-    return config.data
-        .filter(d => {
-            switch(brushGroup.predicate) {
-                case "AND":
-                    return actives.every((p, dimension)=> {
-                        return extents[dimension].some( b => within[config.dimensions[p].type](d,p,dimension,b));
-                    });
-                case "OR":
-                    return actives.some((p, dimension)=> {
-                        return extents[dimension].some(b => within[config.dimensions[p].type](d,p,dimension,b));
-                    });
-                default:
-                    throw new Error("Unknown brush predicate " + config.brushPredicate);
-            }
+  return config.data.filter(d => {
+    switch (brushGroup.predicate) {
+      case 'AND':
+        return actives.every((p, dimension) => {
+          return extents[dimension].some(b =>
+            within[config.dimensions[p].type](d, p, dimension, b)
+          );
         });
+      case 'OR':
+        return actives.some((p, dimension) => {
+          return extents[dimension].some(b =>
+            within[config.dimensions[p].type](d, p, dimension, b)
+          );
+        });
+      default:
+        throw new Error('Unknown brush predicate ' + config.brushPredicate);
+    }
+  });
 };
 
 export default selected;
