5d0a038d5f738c2992d4c5f08596c50e152569cb
Yun Xing <ink.xing.yun@gmail.com>
2018-06-14 16:28:07 -0400

    bump to v2.1.0

--- dist/parcoords.standalone.js
+++ dist/parcoords.standalone.js
@@ -7496,6 +7496,7 @@
         return (end - start) / k;
       });
     };
+    var milliseconds = millisecond.range;
 
     var durationSecond = 1e3;
     var durationMinute = 6e4;
@@ -7512,6 +7513,7 @@
     }, function (date) {
       return date.getUTCSeconds();
     });
+    var seconds = second.range;
 
     var minute = newInterval(function (date) {
       date.setTime(Math.floor(date / durationMinute) * durationMinute);
@@ -7522,6 +7524,7 @@
     }, function (date) {
       return date.getMinutes();
     });
+    var minutes = minute.range;
 
     var hour = newInterval(function (date) {
       var offset = date.getTimezoneOffset() * durationMinute % durationHour;
@@ -7534,6 +7537,7 @@
     }, function (date) {
       return date.getHours();
     });
+    var hours = hour.range;
 
     var day = newInterval(function (date) {
       date.setHours(0, 0, 0, 0);
@@ -7544,6 +7548,7 @@
     }, function (date) {
       return date.getDate() - 1;
     });
+    var days = day.range;
 
     function weekday(i) {
       return newInterval(function (date) {
@@ -7564,6 +7569,8 @@
     var friday = weekday(5);
     var saturday = weekday(6);
 
+    var sundays = sunday.range;
+
     var month = newInterval(function (date) {
       date.setDate(1);
       date.setHours(0, 0, 0, 0);
@@ -7574,6 +7581,7 @@
     }, function (date) {
       return date.getMonth();
     });
+    var months = month.range;
 
     var year = newInterval(function (date) {
       date.setMonth(0, 1);
@@ -7596,6 +7604,7 @@
         date.setFullYear(date.getFullYear() + step * k);
       });
     };
+    var years = year.range;
 
     var utcMinute = newInterval(function (date) {
       date.setUTCSeconds(0, 0);
@@ -7606,6 +7615,7 @@
     }, function (date) {
       return date.getUTCMinutes();
     });
+    var utcMinutes = utcMinute.range;
 
     var utcHour = newInterval(function (date) {
       date.setUTCMinutes(0, 0, 0);
@@ -7616,6 +7626,7 @@
     }, function (date) {
       return date.getUTCHours();
     });
+    var utcHours = utcHour.range;
 
     var utcDay = newInterval(function (date) {
       date.setUTCHours(0, 0, 0, 0);
@@ -7626,6 +7637,7 @@
     }, function (date) {
       return date.getUTCDate() - 1;
     });
+    var utcDays = utcDay.range;
 
     function utcWeekday(i) {
       return newInterval(function (date) {
@@ -7646,6 +7658,8 @@
     var utcFriday = utcWeekday(5);
     var utcSaturday = utcWeekday(6);
 
+    var utcSundays = utcSunday.range;
+
     var utcMonth = newInterval(function (date) {
       date.setUTCDate(1);
       date.setUTCHours(0, 0, 0, 0);
@@ -7656,6 +7670,7 @@
     }, function (date) {
       return date.getUTCMonth();
     });
+    var utcMonths = utcMonth.range;
 
     var utcYear = newInterval(function (date) {
       date.setUTCMonth(0, 1);
@@ -7678,6 +7693,7 @@
         date.setUTCFullYear(date.getUTCFullYear() + step * k);
       });
     };
+    var utcYears = utcYear.range;
 
     function localDate(d) {
       if (0 <= d.y && d.y < 100) {
@@ -9509,7 +9525,7 @@
       };
     };
 
-    var version = "2.0.9";
+    var version = "2.1.0";
 
     var DefaultConfig = {
       data: [],
