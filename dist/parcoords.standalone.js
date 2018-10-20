3c412532d8b211f0ae97296d45ab9b2a9cb3cb8c
Yun Xing <ink.xing.yun@gmail.com>
2018-10-20 16:54:07 -0400

    coding style

--- dist/parcoords.standalone.js
+++ dist/parcoords.standalone.js
@@ -7613,6 +7613,8 @@
     var saturday = weekday(6);
 
     var sundays = sunday.range;
+    var mondays = monday.range;
+    var thursdays = thursday.range;
 
     var month = newInterval(function (date) {
       date.setDate(1);
@@ -7702,6 +7704,8 @@
     var utcSaturday = utcWeekday(6);
 
     var utcSundays = utcSunday.range;
+    var utcMondays = utcMonday.range;
+    var utcThursdays = utcThursday.range;
 
     var utcMonth = newInterval(function (date) {
       date.setUTCDate(1);
