bc27bee166f5d83af4184a01622586e604d5db60
timelyportfolio <kent.russell@timelyportfolio.com>
2018-12-11 21:35:15 -0600

    only use brush.move when setting with brushExtents

--- src/brush/1d/brushExtents.js
+++ src/brush/1d/brushExtents.js
@@ -36,7 +36,10 @@ const brushExtents = (state, config, pc) => extents => {
 
         //update the extent
         //sets the brushable extent to the specified array of points [[x0, y0], [x1, y1]]
-        brush.extent([[-15, yExtent[1]], [15, yExtent[0]]]);
+        //we actually don't need this since we are using brush.move below
+        //extents set the limits of the brush which means a user will not be able
+        //to move or drag the brush beyond the limits set by brush.extent
+        //brush.extent([[-15, yExtent[1]], [15, yExtent[0]]]);
 
         //redraw the brush
         //https://github.com/d3/d3-brush#brush_move
