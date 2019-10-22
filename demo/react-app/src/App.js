da884494ace5536b944e254411b28484a4b145c5
Yun Xing <ink.xing.yun@gmail.com>
2019-10-22 16:09:38 -0400

    demo works

--- demo/react-app/src/App.js
+++ demo/react-app/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import Chart from './chart';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
         <Chart/>
     </div>
   );
--- demo/react-app/src/chart.js
+++ demo/react-app/src/chart.js
@@ -1,26 +1,28 @@
-import React, { useEffect, useRef } from 'react';
+import React, {useEffect, useRef} from 'react';
 import Parcoords from 'parcoord-es';
 import 'parcoord-es/dist/parcoords.css';
 
-const chart = props => {
+const Chart = props => {
     const chartRef = useRef(null);
     useEffect(() => {
         if (chartRef !== null) {
             // use default config;
             const chart = Parcoords()(chartRef.current)
                 .data([
-                    [0,-0,0,0,0,1],
-                    [1,-1,1,2,1,1],
-                    [2,-2,4,4,0.5,1],
-                    [3,-3,9,6,0.33,1],
-                    [4,-4,16,8,0.25,1]
+                    [0, -0, 0, 0, 0, 1],
+                    [1, -1, 1, 2, 1, 1],
+                    [2, -2, 4, 4, 0.5, 1],
+                    [3, -3, 9, 6, 0.33, 1],
+                    [4, -4, 16, 8, 0.25, 1]
                 ])
                 .render()
                 .createAxes();
         }
     }, [chartRef]);
 
-    return(
+    return (
         <div ref={chartRef} id={'chart-id'} style={{width: 600, height: 600}} className={'parcoords'}/>
-);
+    );
 }
+
+export default Chart;
