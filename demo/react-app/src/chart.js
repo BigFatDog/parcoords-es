3d23a383f7bcbde0bd23e5bbed8bf076a0dcbdf8
Hubert Zub <hubert.zub@databricks.com>
2023-01-05 19:41:53 +0100

    revert demo
    Signed-off-by: Hubert Zub <hubert.zub@databricks.com>

--- demo/react-app/src/chart.js
+++ demo/react-app/src/chart.js
@@ -1,38 +1,28 @@
-import React, { useEffect, useRef } from "react";
-import Parcoords from "parcoord-es/dist/parcoords.esm";
-import "parcoord-es/dist/parcoords.css";
+import React, {useEffect, useRef} from 'react';
+import Parcoords from 'parcoord-es';
+import 'parcoord-es/dist/parcoords.css';
 
-const Chart = (props) => {
-  const chartRef = useRef(null);
-  useEffect(
-    () => {
-      if (chartRef !== null) {
-        // use default config;
-        const chart = Parcoords()(chartRef.current)
-          .data([
-            [0, -0, 0, 0, 0, 1],
-            [1, -1, 1, 2, 1, 1],
-            [2, -2, 4, 4, 0.5, 1],
-            [3, -3, 9, 6, 0.33, 1],
-            [4, -4, 16, 8, 0.25, 1],
-          ])
-          .render()
-          .createAxes()
-          .reorderable()
-          .brushMode("1D-axes-multi");
-      }
-    },
-    [chartRef]
-  );
+const Chart = props => {
+    const chartRef = useRef(null);
+    useEffect(() => {
+        if (chartRef !== null) {
+            // use default config;
+            const chart = Parcoords()(chartRef.current)
+                .data([
+                    [0, -0, 0, 0, 0, 1],
+                    [1, -1, 1, 2, 1, 1],
+                    [2, -2, 4, 4, 0.5, 1],
+                    [3, -3, 9, 6, 0.33, 1],
+                    [4, -4, 16, 8, 0.25, 1]
+                ])
+                .render()
+                .createAxes();
+        }
+    }, [chartRef]);
 
-  return (
-    <div
-      ref={chartRef}
-      id={"chart-id"}
-      style={{ width: 600, height: 600 }}
-      className={"parcoords"}
-    />
-  );
-};
+    return (
+        <div ref={chartRef} id={'chart-id'} style={{width: 600, height: 600}} className={'parcoords'}/>
+    );
+}
 
 export default Chart;
